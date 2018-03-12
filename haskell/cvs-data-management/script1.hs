cf73c67ac1a6ffeecbef09deaf5cd596f61175ad
Bjørn Arild Kristiansen <bjornkri@uio.no>
2018-03-12 17:10:46 +0100

    Working on parsing data.

--- haskell/cvs-data-management/script1.hs
+++ haskell/cvs-data-management/script1.hs
@@ -1,14 +1,24 @@
 #!/usr/bin/env stack
--- stack runghc --resolver lts-6.24 --install-ghc --package safe
+-- stack runghc --resolver lts-10.9 --install-ghc --package safe --package split
 
 import Data.List
+import Data.List.Split
 import Safe
 import Data.Either
 import Control.Monad
 
+data DataSet = DataSet HeaderRow [DataRow]
+data HeaderRow = HeaderRow [HeaderVar]
+data DataRow = DataRow [DataVar]
 data Error = Error String deriving (Show)
-data HeaderVar = HeaderVar String deriving (Show, Eq)
-data DataVar = DataVar String deriving (Show)
+data HeaderVar = HeaderVar String deriving (Eq)
+data DataVar = DataVar String 
+
+instance Show DataVar where
+    show (DataVar s) = "D " ++ s
+instance Show HeaderVar where
+    show (HeaderVar s) = s
+
 data Id = Id String deriving (Show)
 data IdIndex = IdIndex Int deriving (Show)
 
@@ -24,6 +34,16 @@ dataz = [
 
 -- indexOf :: String -> [HeaderVar] -> Maybe Int
 -- indexOf headerVars S = atMay headerVars s
+
+
+
+
+
+
+-- ════════════════════════════════════════════════════════════════════════════════════
+-- ░▒▓▄   DATA MANAGEMENT   ▀▓▒░
+-- ════════════════════════════════════════════════════════════════════════════════════
+
 
 idIndex :: [HeaderVar] -> Either Error IdIndex
 idIndex hs = 
@@ -50,27 +70,89 @@ extractDataForCase ds (IdIndex idx) (Id id0) =
         Just h -> Right $ map DataVar h
 
         
+selectVars :: [HeaderVar] -> [HeaderVar] -> [DataVar] -> Either Error [DataVar]
+selectVars vars header data0 =
+    let busque var = 
+         case elemIndex var header of
+            Nothing -> Left $ Error ("Variable does not exist: " ++ show var ++ ".")
+            Just a -> Right a
+    in
+    
+    let indexes = mapM busque vars in
+    (fmap . map) (data0 !!) indexes
 
 
-
-extractDataForCase  = do
+extractDataForCasexz = do
     h <- extractHeader dataz
     idx <- idIndex h
     d <- extractDataForCase dataz idx (Id "200BB")
     return d
 
 
-selectVars :: [HeaderVar] -> [HeaderVar] -> [DataVar]
-filterVars vars header data0 =
-    let busque var = case atMay header var of
-        Nothing -> Left $ Error "Invalid variable: " ++ var
-        Just a -> Right a
-    
-    let indexes = mapM busque data0
+
+header = Data.Either.fromRight [] (extractHeader dataz)
+
+someCase = fromRight [] extractDataForCasexz
 
 
 
-    map 
+
+
+-- ════════════════════════════════════════════════════════════════════════════════════
+-- ░▒▓▄   PARSE DATA FROM FILE   ▀▓▒░
+-- ────────────────────────────────────────────────────────────────────────────────────
+-- Assuming csv-data with
+-- * Separator: ';'
+-- * Header row
+-- * No quoted strings
+-- * No semi-colon in strings
+-- ════════════════════════════════════════════════════════════════════════════════════
+
+
+toDataSet :: [[String]] -> Either Error DataSet
+toDataSet [] = Left $ Error("Cannot create dataset: No source data.")
+toDataSet (headerRow:dataRows) =    
+
+    let 
+        toDataRow :: [String] -> DataRow
+        toDataRow s = DataRow (map DataVar s)
+
+        proc :: [[String]] -> [DataRow]
+        proc [] = []
+        proc (a:as) = (toDataRow a) : proc(as)
+    in
+        Right $ DataSet (HeaderRow (map HeaderVar headerRow)) (proc dataRows)
+
+
+
+parse :: String -> Either Error DataSet
+parse s =
+    let s1 = lines s
+        s2 = map (splitOn ";") s1
+    in
+    toDataSet s2
+
+
+--something fileName = do
+--    fileContents <- readFile fileName
+
+
+--parse :: Handle -> IO [CbclRow]
+--parse inHandle rows = do
+--    readFile ""
+--    isEof <- hIsEOF inHandle
+--    if isEof
+--    then 
+--        return rows
+--    else do
+--        row <- hGetLine inHandle
+--        parse inHandle ((splitRow row):rows)
+
+
+
+
+
+
 
 
 
